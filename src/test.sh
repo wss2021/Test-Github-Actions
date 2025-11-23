9744d75256a7758ee4641632f6de5df04adb9d10
Wadhah Sebti <Wadhahss@gmail.com>
2025-11-23 14:25:24 +0300

    Correct Code

--- src/test.sh
+++ src/test.sh
@@ -2,10 +2,17 @@
 
 #src/test.sh
 EXPECTED="HEllo, Test!"
+
 OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
-if [ "$OUTPUT" == "$EXPECTED"]: then
-echp "Test Passed"
+
+if [ "$OUTPUT" == "$EXPECTED"]; then
+
+echo "Test Passed"
+
 exit 0
+
 else
+
 echo "Test faild! Expected '$EXPECTED' but got '$OUTPUT'"
+
 exit 1

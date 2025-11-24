54a9109f7d4c97c033ca7ca4c84ddbbc47f80e50
Wadhah Sebti <Wadhahss@gmail.com>
2025-11-24 18:41:56 +0300

    Test change in test branch

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ function greet(name) {
     return `Hello, ${name}!`;
 }
 
-module.exports = greet;
+// module.exports = greet;
 
-if (require.main === module) {
-    console.log(greet("world"));
-}
+// if (require.main === module) {
+//     console.log(greet("world"));
+// }
--- src/test.sh
+++ src/test.sh
@@ -5,9 +5,9 @@ EXPECTED="Hello, Test!"
 OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
-  echo "Test Passed"
+  echo "✅ Test Passed"
   exit 0
 else
-  echo "Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+  echo "❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
   exit 1
 fi
